349ca63302f2514d4aeb3d295051cd1d621ce563
Yago Mathias <88664717+yagomathiass@users.noreply.github.com>
2021-09-21 11:25:05 -0300

    Add files via upload

---
+++ implInterface.ts
@@ -0,0 +1,102 @@
+// interface UserProps{
+//     firtsName: string
+//     lastName: string
+//     username: string
+//     email: string
+//     password: string
+//     cpf: number
+//     contacto: number
+//     isAdmin: boolean
+// };
+
+// class User implements UserProps{
+//     firtsName!: string
+//     lastName!: string
+//     username!: string
+//     email!: string
+//     password!: string
+//     cpf!: number
+//     contacto!: number
+//     isAdmin!: boolean
+
+//         constructor(firstName: string, lastName:string, username:string, email:string, password:string, cpf:number, contacto:number, isAdmin:boolean){
+//             this.firtsName = firstName
+//             this.lastName = lastName
+//             this.username = username
+//             this.email = email
+//             this.password = password
+//             this.cpf = cpf
+//             this.contacto = contacto
+//            this.isAdmin = isAdmin
+//         }
+        
+//         //name-------------------
+//         getFirstName(){
+//             return this.firtsName
+//         }
+//         setFirstName(firstName:string){
+//             this.firtsName = firstName
+//         }
+
+//         //lastname-------------------
+//         getLastName(){
+//             return this.lastName
+//         }
+//         setLastName(lastName:string){
+//             this.lastName = lastName
+//         }
+
+//         //username-------------------
+//         getUsername(){
+//             return this.username
+//         }
+//         setUsername(username:string){
+//             this.username = username
+//         }
+
+
+//         //email-------------------
+//         getEmail(){
+//             return this.email
+//         }
+//         setEmail(email:string){
+//             this.email = email
+//         }
+
+//         //password-------------------
+//         getPassword(){
+//             return this.password
+//         }
+//         setPassword(password:string){
+//             this.password = password
+//         }
+
+
+//         //cpf-------------------
+//         getCpf(){
+//             return this.cpf
+//         }
+//         setCpf(cpf:number){
+//             this.cpf = cpf
+//         }
+
+//         //contacto-------------------
+//         getContacto(){
+//             return this.contacto
+//         }
+//         setContacto(contacto:number){
+//             this.contacto = contacto
+//         }
+
+//         //isadmin-------------------
+//         getIsAdmin(){
+//             return this.isAdmin
+//         }
+//         setIsAdmin(isAdmin:boolean){
+//             this.isAdmin = isAdmin
+//         }
+// };
+
+// let user = new User("Yago","Mathias","yaguera","yago_mathias@outloo.com","****",2135523, 1192384767, true)
+
+// console.log(user)
